e4b3ca3782825791fcc11a90c74b599b5a5958cb
Hitinbajaj <hitinbajaj77@gmail.com>
2024-01-19 04:58:44 +0530

    updated url cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://e-commerce-skr-frontend.vercel.app/"],
+    origin: ["https://e-commerce-skr-frontend.vercel.app"],
     methods: ["POST", "GET", "DELETE"],
     credentials: true
   }
